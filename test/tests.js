72848f60c9a837900e284856f102e957b69960aa
DannySuarez <suarezd10@gmail.com>
2019-05-13 09:57:15 -0700

    starter files

---
+++ test/tests.js
@@ -0,0 +1,16 @@
+const test = QUnit.test;
+
+QUnit.testStart(() => {
+    sessionStorage.clear();
+});
+
+test('test', (assert) => {
+    // arrange
+    
+
+    // act
+    
+
+    // assert
+    assert.deepEqual(result, expected);
+});
