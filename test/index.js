cf6f3c952061987b7903d315cf6fe11abd86cd21
Carlos Us Santiago <cdus12@gmail.com>
2019-05-13 14:31:36 -0700

    updated configs

--- test/index.js
+++ test/index.js
@@ -11,6 +11,7 @@ global.document = window.document;
 global.FormData = window.FormData;
 global.localStorage = window.localStorage;
 global.sessionStorage = window.sessionStorage;
+global.URLSearchParams = window.URLSearchParams;
 
 require = require('esm')(module);
 module.exports = require('./tests.js');
--- test/tests.js
+++ test/tests.js
@@ -1,16 +1,12 @@
-// const test = QUnit.test;
+const test = QUnit.test;
 
-// QUnit.testStart(() => {
-//     sessionStorage.clear();
-// });
+test('time to test a function', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
 
-// test('test', (assert) => {
-//     // arrange
-    
+    //Act 
+    // Call the function you're testing and set the result to a const
 
-//     // act
-    
-
-//     // assert
-//     assert.deepEqual(result, expected);
-// });
+    //Assert
+    assert.equal(true, false);
+});
