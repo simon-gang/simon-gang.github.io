ce1cc9e75a33081130ff47ee5adc12bc7c96b3c1
Dirt Deodara <thecrowleaps@gmail.com>
2019-05-14 16:19:21 -0700

    after a long day ofsemi-successful debugging, we now move on to building out our other pages.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
-
 import turnAllOff from './turn-all-off.js';
 
 const startButton = document.getElementById('start-button');
 const lights = document.querySelectorAll('.light');
-let count = 3;
+const orbsContainer = document.getElementById('orbs')
+let count = 2;
 let currentSequence;
 
+
+//generating the computer array of colors
 function genSequence() {
     const sequence = [];
     for(let i = 0; i < count; i++) {
+        console.log('looping over index of' + i + ' count is ' + count);
         sequence.push(Math.floor(Math.random() * 4));
     }
     currentSequence = sequence;
+    console.log(currentSequence, 'generated sequesnce');
     return currentSequence;
 }
 
+//this is making the lights light up depending on thier possition in the array
 function playSequence(sequence) {
+    console.log('play sequence ', sequence);
     
     let i = 0;
     
@@ -39,15 +45,20 @@ function playSequence(sequence) {
     }, 1250);
 }
 
+//starts the play function
 startButton.addEventListener('click', () => {
     const sequence = genSequence();
-    count++;
+    console.log('The sequence in the start button' + sequence);
+    orbsContainer.classList.remove('disable');
+    // count++; 
 
     playSequence(sequence);
-    console.log(sequence);
 });
 
+
+//tracks the clicks of the user
 function trackClicks(sequence) {
+    console.log(sequence, 'tracking user clicks');
 
     let position = 0;
 
@@ -59,13 +70,32 @@ function trackClicks(sequence) {
                 currentElement.classList.remove('on');
                 const correct = sequence[position];
                 const guess = i;
-                console.log(correct, guess);
+                console.log('comparing correct ' + correct + ' and user click ' + guess);
                 position++;
+                console.log('position is at ' + position)
+                compare(correct, guess);
             }, 500); 
         });
     }
 }
 
+let playCount = 0;
 
+function compare(correct, guess) {
+    playCount++;
+    if(correct !== guess) {
+        donePlaying();
+    } 
+    else if(currentSequence.length === playCount) {
+        orbsContainer.classList.add('disable');
+        startButton.innerHTML = 'Next Level';
+        count++; 
+    }
+}
+
+function donePlaying() {
+    window.alert('Incorrect! Restart your game.')
+    count = 2;
+}
 
 export default playSequence;
