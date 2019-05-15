fac21cbbdb99342fa6e3eb5058f61403f786d9f3
Dirt Deodara <thecrowleaps@gmail.com>
2019-05-15 12:00:23 -0700

    git simon game working

--- src/index.js
+++ src/index.js
@@ -5,24 +5,20 @@ const startButton = document.getElementById('start-button');
 const lights = document.querySelectorAll('.light');
 const orbsContainer = document.getElementById('orbs')
 let count = 2;
-let currentSequence;
-let playCount = 0;
+let sequence;
+let tracking =  false;
 
 //generates the computer array of numbers
 function genSequence() {
-    const sequence = [];
+    sequence = [];
     for(let i = 0; i < count; i++) {
         console.log('looping over index of' + i + ' count is ' + count);
         sequence.push(Math.floor(Math.random() * 4));
     }
-    // find if we can just return currentSequence
-    currentSequence = sequence;
-    console.log(currentSequence, 'generated sequesnce');
-    return currentSequence;
 }
 
 //this is making the lights light up depending on their position in the array
-function playSequence(sequence) {
+function playSequence() {
     console.log('play sequence ', sequence);
     
     let i = 0;
@@ -32,7 +28,7 @@ function playSequence(sequence) {
         
         if(i === sequence.length) {
             clearInterval(interval);
-            trackClicks(sequence);
+            tracking = true;
         }
         else {
             setTimeout(() => {
@@ -47,53 +43,46 @@ function playSequence(sequence) {
     }, 1050);
 }
 
+let position = 0;
 //starts the play function
 startButton.addEventListener('click', () => {
-    const sequence = genSequence();
-    console.log('The sequence in the start button' + sequence);
+    genSequence();
     orbsContainer.classList.remove('disable');
-    // count++; 
-    playSequence(sequence);
+    playSequence();
+    position = 0;
 });
-
-//tracks the clicks of the user
-function trackClicks(sequence) {
-    console.log(sequence, 'tracking user clicks');
-
-    let position = 0;
-
-    for(let i = 0; i < lights.length; i++) {
-        const currentElement = lights[i];
-        currentElement.addEventListener('click', (event) => {
-            currentElement.classList.add('on');
-            setTimeout(() => {
-                currentElement.classList.remove('on');
-                const correct = sequence[position]; //use currentSequence here instead of sequence and maybe everywhere else, too.
-                const guess = i;
-                console.log('comparing correct ' + correct + ' and user click ' + guess);
-                position++;
-                console.log('position is at ' + position)
-                compare(correct, guess);
-            }, 500); 
-        });
-    }
+    
+for(let i = 0; i < lights.length; i++) {
+    const currentElement = lights[i];
+    currentElement.addEventListener('click', () => {
+        if(!tracking) {
+            return;
+        }
+        currentElement.classList.add('on');
+        setTimeout(() => {
+            currentElement.classList.remove('on');
+            const correct = sequence[position]; //use currentSequence here instead of sequence and maybe everywhere else, too.
+            const guess = i;
+            console.log('comparing correct ' + correct + ' and user click ' + guess);
+            position++;
+            console.log('position is at ' + position)
+            compare(correct, guess);
+        }, 500); 
+    });
 }
 
 function compare(correct, guess) {
-    playCount++;
+    console.log(correct, guess, 'inside compare');
     if(correct !== guess) {
-        donePlaying();
+        count = 2;
+        tracking = false;
     } 
-    else if(currentSequence.length === playCount) {
+    else if(sequence.length === position) {
         orbsContainer.classList.add('disable');
         startButton.innerHTML = 'Next Level';
         count++; 
+        tracking = false;
     }
-}
-
-function donePlaying() {
-    window.alert('Incorrect! Restart your game.')
-    count = 2;
 }
 
 export default playSequence;
