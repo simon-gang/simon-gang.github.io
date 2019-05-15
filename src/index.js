9e800fc8b8307f3db0c824cadd940ce1582ee078
Dirt Deodara <thecrowleaps@gmail.com>
2019-05-15 13:53:01 -0700

    cleaned up code removed console logs

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
 import turnAllOff from './turn-all-off.js';
-
 // declaring global variables and referencing DOM elements
 const startButton = document.getElementById('start-button');
 const lights = document.querySelectorAll('.light');
-const orbsContainer = document.getElementById('orbs')
 let count = 2;
 let sequence;
-let tracking =  false;
+let tracking = false;
+let position = 0;
 
 //generates the computer array of numbers
-function genSequence() {
+function genSequence() { //rename
     sequence = [];
     for(let i = 0; i < count; i++) {
-        console.log('looping over index of' + i + ' count is ' + count);
         sequence.push(Math.floor(Math.random() * 4));
     }
 }
 
 //this is making the lights light up depending on their position in the array
 function playSequence() {
-    console.log('play sequence ', sequence);
     
     let i = 0;
     
@@ -43,11 +40,9 @@ function playSequence() {
     }, 1050);
 }
 
-let position = 0;
 //starts the play function
 startButton.addEventListener('click', () => {
     genSequence();
-    orbsContainer.classList.remove('disable');
     playSequence();
     position = 0;
 });
@@ -61,28 +56,23 @@ for(let i = 0; i < lights.length; i++) {
         currentElement.classList.add('on');
         setTimeout(() => {
             currentElement.classList.remove('on');
-            const correct = sequence[position]; //use currentSequence here instead of sequence and maybe everywhere else, too.
+            const correct = sequence[position]; 
             const guess = i;
-            console.log('comparing correct ' + correct + ' and user click ' + guess);
             position++;
-            console.log('position is at ' + position)
             compare(correct, guess);
         }, 500); 
     });
 }
 
 function compare(correct, guess) {
-    console.log(correct, guess, 'inside compare');
     if(correct !== guess) {
         count = 2;
         tracking = false;
     } 
     else if(sequence.length === position) {
-        orbsContainer.classList.add('disable');
         startButton.innerHTML = 'Next Level';
         count++; 
         tracking = false;
     }
 }
 
-export default playSequence;
