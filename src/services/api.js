c76898f78177562c4e674484a8e69e61b061a3e9
Carlos Us Santiago <cdus12@gmail.com>
2019-05-16 09:45:51 -0700

    removed console logs from api

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,6 @@ const api = {
     storage: localStorage,
 
     saveUser(player){
-        console.log(player);
         const json = JSON.stringify(player);
         api.storage.setItem('player', json);
         // const players = api.getAll();
@@ -16,30 +15,6 @@ const api = {
         return player;
     }
 
-
-
-
-
-    
-    // },
-    // saveNewUser(user){
-    //     const players = api.getAll();
-    //     players.unshift(user);
-    //     const json = JSON.stringify(players);
-    //     api.storage.setItem(api.key, json);
-    // },
-    // getUser(){
-    //     const players = api.getAll();
-    //     return players[0];
-    // },
-    // getAll() {
-    //     const json = localStorage.getItem(api.key);
-    //     let players = JSON.parse(json);
-    //     if(!players) {
-    //         players = [];
-    //     }
-    //     return players;
-    // },
    
 };
 
